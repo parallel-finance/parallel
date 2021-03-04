a47b8cac30d813eb3ed3104620e4ef0e8a782029
HZ <haiyizxx@gmail.com>
2021-03-04 02:37:47 -0500

    Add function for other pallets to call

--- pallets/rate/src/lib.rs
+++ pallets/rate/src/lib.rs
@@ -1,20 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch, traits::Get};
+use frame_support::{decl_error, decl_event, decl_module, decl_storage, dispatch};
 use frame_system::ensure_signed;
+use primitives::Balance;
 use primitives::CurrencyId;
-use primitives::{Amount, Balance};
-use sp_core::U256;
-use sp_runtime::{
-    traits::{One, Zero},
-    DispatchError, DispatchResult, FixedPointNumber, FixedU128, ModuleId, RuntimeDebug,
-    SaturatedConversion,
-};
-use sp_std::{convert::TryInto, prelude::*, vec};
+use sp_runtime::traits::Zero;
+use sp_std::prelude::*;
+
 const BLOCK_PER_YEAR: u128 = 5256000;
+// const BLOCK_PER_YEAR: u128 = 2102400;
 const DECIMAL: u128 = 1_000_000_000_000_000_000;
-pub type Ratio = FixedU128;
-pub type Rate = FixedU128;
 
 pub trait Config: frame_system::Config {
     type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
@@ -39,13 +34,13 @@ decl_event!(
     where
         AccountId = <T as frame_system::Config>::AccountId,
     {
-        NewInterestParams(AccountId, u128, u128, u128, u128),
+        NewInterestParams2(AccountId, u128, u128, u128, u128),
+        NewInterestParams(u128, u128, u128, u128),
         BorrowRateUpdated(CurrencyId, u128),
         SupplyRateUpdated(CurrencyId, u128),
         UtilityRateUpdated(CurrencyId, u128),
-		Test(u128),
-	}
-
+        Test(u128),
+    }
 );
 
 decl_error! {
@@ -61,7 +56,7 @@ decl_module! {
         fn deposit_event() = default;
 
         #[weight = 10_000]
-        pub fn update_jump_rate_model(
+        pub fn update_jump_rate_model_tx(
             origin,
             base_rate_per_year: Balance,
             multiplier_per_year: Balance,
@@ -91,7 +86,6 @@ decl_module! {
             Kink::put(kink);
 
             Self::deposit_event(RawEvent::NewInterestParams(
-                who,
                 base,
                 multiplier,
                 jump,
@@ -102,11 +96,11 @@ decl_module! {
 
 
         #[weight = 10_000]
-        pub fn update_borrow_rate(origin, currency_id: CurrencyId, cash: Balance, borrows: Balance, reserves: Balance) -> dispatch::DispatchResult {
+        pub fn update_borrow_rate_tx(origin, currency_id: CurrencyId, cash: Balance, borrows: Balance, reserves: Balance) -> dispatch::DispatchResult {
             let _who = ensure_signed(origin)?;
 
             let util = Self::utilization_rate
-			(cash, borrows, reserves);
+            (cash, borrows, reserves);
             UtilityRate::insert(currency_id, util);
             Self::deposit_event(RawEvent::UtilityRateUpdated(currency_id, util));
 
@@ -117,30 +111,30 @@ decl_module! {
 
             if util <= kink {
                 let rate = util.checked_mul(multiplier_per_block)
-					.and_then(|r| r.checked_div(DECIMAL))
+                    .and_then(|r| r.checked_div(DECIMAL))
                     .and_then(|r| r.checked_add(base_rate_per_block))
                     .unwrap();
-                Self::deposit_event(RawEvent::Test(11));
-				Self::insert_borrow_rate(currency_id, rate);
+
+                Self::insert_borrow_rate(currency_id, rate);
 
             } else {
                 let normal_rate = kink.checked_mul(multiplier_per_block)
-				.and_then(|r| r.checked_div(DECIMAL))
-				.and_then(|r| r.checked_add(base_rate_per_block)).unwrap();
+                .and_then(|r| r.checked_div(DECIMAL))
+                .and_then(|r| r.checked_add(base_rate_per_block)).unwrap();
 
                 let excess_util = util.saturating_sub(kink);
                 let rate = excess_util
                     .checked_mul(jump_multiplier_per_block)
                     .and_then(|r| r.checked_add(normal_rate))
                     .unwrap();
-				Self::deposit_event(RawEvent::Test(12));
+
                 Self::insert_borrow_rate(currency_id, rate);
             }
             Ok(())
         }
 
         #[weight = 10_000]
-        pub fn update_supply_rate(
+        pub fn update_supply_rate_tx(
             origin,
             currency_id: CurrencyId,
             cash: Balance,
@@ -150,12 +144,12 @@ decl_module! {
         ) -> dispatch::DispatchResult {
             let _who = ensure_signed(origin)?;
             let one_minus_reserve_factor = u128::from(DECIMAL).saturating_sub(reserve_factor_mantissa);
-			Self::deposit_event(RawEvent::Test(one_minus_reserve_factor));
+
             let borrow_rate =  BorrowRate::get(currency_id);
             let rate_to_pool = Self::to_decimal(borrow_rate.checked_mul(one_minus_reserve_factor));
-			Self::deposit_event(RawEvent::Test(rate_to_pool));
+
             let rate = Self::to_decimal(Self::utilization_rate
-				(cash, borrows, reserves)
+                (cash, borrows, reserves)
                 .checked_mul(rate_to_pool));
             Self::insert_supply_rate(currency_id, rate);
             Ok(())
@@ -178,8 +172,7 @@ impl<T: Config> Module<T> {
         n.and_then(|r| r.checked_div(DECIMAL)).unwrap()
     }
 
-    fn utilization_rate
-	(cash: Balance, borrows: Balance, reserves: Balance) -> u128 {
+    pub fn utilization_rate(cash: Balance, borrows: Balance, reserves: Balance) -> u128 {
         // Utilization rate is 0 when there are no borrows
         if borrows.is_zero() {
             return Zero::zero();
@@ -194,5 +187,92 @@ impl<T: Config> Module<T> {
             .checked_mul(DECIMAL)
             .and_then(|r| r.checked_div(total))
             .unwrap()
+    }
+
+    pub fn update_jump_rate_model(
+        base_rate_per_year: Balance,
+        multiplier_per_year: Balance,
+        jump_multiplier_per_year: Balance,
+        kink: Balance,
+    ) -> dispatch::DispatchResult {
+        let base = base_rate_per_year.checked_div(BLOCK_PER_YEAR).unwrap();
+
+        let multiplier = multiplier_per_year
+            .checked_mul(DECIMAL)
+            .and_then(|r| r.checked_div(BLOCK_PER_YEAR.checked_mul(kink).unwrap()))
+            .unwrap();
+
+        let jump = jump_multiplier_per_year
+            .checked_div(BLOCK_PER_YEAR)
+            .unwrap();
+
+        BaseRatePerBlock::put(base);
+        MultiplierPerBlock::put(multiplier);
+        JumpMultiplierPerBlock::put(jump);
+        Kink::put(kink);
+
+        Self::deposit_event(RawEvent::NewInterestParams(base, multiplier, jump, kink));
+        Ok(())
+    }
+
+    pub fn update_borrow_rate(
+        currency_id: CurrencyId,
+        cash: Balance,
+        borrows: Balance,
+        reserves: Balance,
+    ) -> dispatch::DispatchResult {
+        let util = Self::utilization_rate(cash, borrows, reserves);
+        UtilityRate::insert(currency_id, util);
+        Self::deposit_event(RawEvent::UtilityRateUpdated(currency_id, util));
+
+        let multiplier_per_block = MultiplierPerBlock::get().unwrap();
+        let base_rate_per_block = BaseRatePerBlock::get().unwrap();
+        let kink = Kink::get().unwrap();
+        let jump_multiplier_per_block = Self::to_decimal(JumpMultiplierPerBlock::get());
+
+        if util <= kink {
+            let rate = util
+                .checked_mul(multiplier_per_block)
+                .and_then(|r| r.checked_div(DECIMAL))
+                .and_then(|r| r.checked_add(base_rate_per_block))
+                .unwrap();
+
+            Self::insert_borrow_rate(currency_id, rate);
+        } else {
+            let normal_rate = kink
+                .checked_mul(multiplier_per_block)
+                .and_then(|r| r.checked_div(DECIMAL))
+                .and_then(|r| r.checked_add(base_rate_per_block))
+                .unwrap();
+
+            let excess_util = util.saturating_sub(kink);
+            let rate = excess_util
+                .checked_mul(jump_multiplier_per_block)
+                .and_then(|r| r.checked_add(normal_rate))
+                .unwrap();
+
+            Self::insert_borrow_rate(currency_id, rate);
+        }
+        Ok(())
+    }
+
+    pub fn update_supply_rate(
+        currency_id: CurrencyId,
+        cash: Balance,
+        borrows: Balance,
+        reserves: Balance,
+        reserve_factor_mantissa: Balance,
+    ) -> dispatch::DispatchResult {
+        let one_minus_reserve_factor = u128::from(DECIMAL).saturating_sub(reserve_factor_mantissa);
+
+        let borrow_rate = BorrowRate::get(currency_id);
+        let rate_to_pool = Self::to_decimal(borrow_rate.checked_mul(one_minus_reserve_factor));
+
+        let rate = Self::to_decimal(
+            Self::utilization_rate(cash, borrows, reserves).checked_mul(rate_to_pool),
+        );
+        Self::insert_supply_rate(currency_id, rate);
+
+        Ok(())
     }
 }
