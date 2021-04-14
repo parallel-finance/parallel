0978fcbd0bd5f832bd27da8d5f01727fa33f600e
Cheng JIANG <alex_cj96@foxmail.com>
2021-04-14 14:27:02 +0800

    update lockfile

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 cd $DIR/..
-
-echo "*** Start parallel node ***"
-
-docker-compose down --remove-orphans
-docker-compose up -d
-docker-compose logs -f
