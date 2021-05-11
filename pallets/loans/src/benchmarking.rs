0d48f7cbb829b09a44bcc23ac49174560cbf9218
ayushmishra2005 <ayushmishra2005@gmail.com>
2021-05-11 13:24:17 +0530

    Added benchmarking for `redeem` #34

--- pallets/loans/src/benchmarking.rs
+++ pallets/loans/src/benchmarking.rs
@@ -10,7 +10,9 @@ pub const DOT: CurrencyId = CurrencyId::DOT;
 pub const INITIAL_AMOUNT: u128 = 10000;
 
 fn initial_set_up<T: Config>(caller: T::AccountId) {
+    let account_id = Loans::<T>::account_id();
     T::Currency::deposit(DOT, &caller, INITIAL_AMOUNT).unwrap();
+    T::Currency::deposit(DOT, &account_id, INITIAL_AMOUNT).unwrap();
 }
 
 benchmarks! {
@@ -40,6 +42,20 @@ benchmarks! {
             INITIAL_AMOUNT - borrowed_amount,
         );
         assert_eq!(Loans::<T>::account_borrows(DOT, caller).principal, borrowed_amount);
+    }
+
+    redeem {
+        let caller: T::AccountId = whitelisted_caller();
+        initial_set_up::<T>(caller.clone());
+        <AccountCollateral<T>>::insert(DOT, caller.clone(), 100_000);
+        <TotalSupply<T>>::insert(DOT, 100_000);
+        let amount = 100;
+    }: _(SystemOrigin::Signed(caller.clone()), DOT, amount)
+    verify {
+        assert_eq!(
+            <T as Config>::Currency::free_balance(DOT, &Loans::<T>::account_id()),
+            INITIAL_AMOUNT - amount,
+        );
     }
 }
 
