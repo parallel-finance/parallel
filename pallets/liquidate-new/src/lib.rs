d69e1f08f70982b911fbb1ea50219b8b2957d985
Kaichao Sun <kaichaosuna@gmail.com>
2021-05-21 15:20:54 +0800

    more

--- pallets/liquidate-new/src/lib.rs
+++ pallets/liquidate-new/src/lib.rs
@@ -23,13 +23,16 @@
 use sp_core::{
 	crypto::KeyTypeId,
 };
-use sp_runtime::offchain::{
-	Duration,
-	storage_lock::{BlockNumberProvider, StorageLock, Time}
+use sp_runtime::{
+	FixedPointNumber, Percent,
+	offchain::{
+		Duration,
+		storage_lock::{StorageLock, Time}
+	}
 };
 use sp_std::collections::btree_map::BTreeMap;
 use frame_support::{
-	dispatch::DispatchResultWithPostInfo,
+	// dispatch::DispatchResultWithPostInfo,
 	pallet_prelude::*,
 	log,
 };
@@ -40,6 +43,7 @@ pub use pallet::*;
 
 pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"liqu");
 pub const LOCK_PERIOD: u64 = 20000; // in milli-seconds
+pub const LIQUIDATE_FACTOR: Percent = Percent::from_percent(50); // 0.5
 
 pub enum Error {
 	/// There is no pre-configured currencies
@@ -49,9 +53,9 @@ pub mod crypto {
 	use super::KEY_TYPE;
 	use sp_runtime::{
 		app_crypto::{app_crypto, sr25519},
-		traits::Verify,
+		// traits::Verify,
 	};
-	use sp_core::sr25519::Signature as Sr25519Signature;
+	// use sp_core::sr25519::Signature as Sr25519Signature;
 	app_crypto!(sr25519, KEY_TYPE);
 
 	// pub sturct TestAuthId;
@@ -67,7 +71,8 @@ pub mod pallet {
 	use super::*;
 
 	#[pallet::config]
-	pub trait Config: frame_system::Config + pallet_loans::Config {}
+	pub trait Config: frame_system::Config + pallet_loans::Config {
+	}
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -104,48 +109,52 @@ impl<T: Config> Pallet<T> {
 		
 
 		let aggregated_account_borrows = pallet_loans::AccountBorrows::<T>::iter()
-			.fold(BTreeMap::new(), |acc, (k1, k2, balance)| {
-				let loans_value = balance.principal * T::PriceFeeder::get_price(k1)::unwrap();
-				let previous_value = acc.get(&k2).unwrap();
-				let total_loans = previous_value._1 + loans_value;
-				let loans_detail = previous_value._2.push((k1, balance));
-				acc.insert(k2, (total_loans, loans_detail));
+			.fold(BTreeMap::<T::AccountId, (Balance, Vec<(CurrencyId, Balance)>)>::new(), |acc, (k1, k2, snapshot)| {
+				let loans_value = T::PriceFeeder::get_price(&k1).unwrap().0.checked_mul_int(snapshot.principal).unwrap();
+				let existing = acc.get(&k2).unwrap();
+				let total_loans_value = existing.0 + loans_value;
+				let mut loans_detail = existing.1;
+				loans_detail.push((k1, snapshot.principal));
+				acc.insert(k2, (total_loans_value, loans_detail));
 				acc
 			});
 
 		let aggregated_account_collatoral = pallet_loans::AccountCollateral::<T>::iter()
-			.fold(BTreeMap::new(), |acc, (k1, k2, balance)| {
-				let collatoral_value = balance * T::PriceFeeder::get_price(&k1).unwrap();
-				let previous_value = acc.get(&k2).unwrap();
-				let totoal_collatoral_value = previous_value._1 + collatoral_value;
-				let collatoral_detail = previous_value._2.push((k1, balance));
+			.fold(BTreeMap::<T::AccountId, (Balance, Vec<(CurrencyId, Balance)>)>::new(), |acc, (k1, k2, balance)| {
+				let collatoral_value = pallet_loans::CollateralFactor::<T>::get(&k1).mul_floor(
+					T::PriceFeeder::get_price(&k1).unwrap().0.checked_mul_int(balance).unwrap()
+				);
+				let existing = acc.get(&k2).unwrap();
+				let totoal_collatoral_value = existing.0 + collatoral_value;
+				let mut collatoral_detail = existing.1;
+				collatoral_detail.push((k1, balance));
 				acc.insert(k2, (totoal_collatoral_value, collatoral_detail));
 				acc
 			});
 
 		let underwater_account_borrows = aggregated_account_borrows.iter()
-			.filter(|(account, (loans, _))| {
-				loans > aggregated_account_collatoral.get(account).unwrap()._1
+			.filter(|(account, (total_loans_value, _))| {
+				total_loans_value > &aggregated_account_collatoral.get(account).unwrap().0
 			})
-			.map(|account, (total_loans_value, loans_detail)| {
+			.map(|(account, (total_loans_value, loans_detail))| {
 				// TODO change to 0.5, configurable by runners
 				// TODO shortfall compare with 0.5 * max
-				let liquidation_value = total_loans_value * 0.5;
-				let liquidation_loan = loans_detail.find(
-					|(currency, balance)| balance * T::PriceFeeder::get_price(currency).unwrap() >= liquidation_value
+				let liquidation_value = LIQUIDATE_FACTOR.mul_floor(*total_loans_value);
+				let liquidation_loan = (*loans_detail).find(
+					|(currency, balance)| T::PriceFeeder::get_price(currency).unwrap().0.checked_mul_int(balance) >= liquidation_value
 				);
 				let liquidation_collatoral = 
 					aggregated_account_collatoral
 					.get(account)
 					.unwarp() // TODO several assets to liquidation
-					._2
+					.1
 					.find(|(currency, balance)| 
-						balance * T::PriceFeeder::get_price(currency).unwrap() >= liquidation_loan
+						T::PriceFeeder::get_price(currency).unwrap().0.checked_mul_int(balance) >= liquidation_loan
 					);
 				(
 					account,
-					liquidation_loan.unwrap()._1, 
-					liquidation_collatoral.unwrap()._1, 
+					liquidation_loan.unwrap().0,
+					liquidation_collatoral.unwrap().0, 
 					liquidation_value,
 				)
 			})
