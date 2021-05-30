4e38dda51100b43daa5fcfea739c99eaf6b1201c
Cheng JIANG <alex_cj96@foxmail.com>
2021-05-30 12:47:51 +0800

    use 12 secs as target block producing time

--- runtime/parallel/src/lib.rs
+++ runtime/parallel/src/lib.rs
@@ -124,7 +124,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 /// up by `pallet_aura` to implement `fn slot_duration()`.
 ///
 /// Change this to adjust the block time.
-pub const MILLISECS_PER_BLOCK: u64 = 6000;
+pub const MILLISECS_PER_BLOCK: u64 = 12000;
 
 pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
 
