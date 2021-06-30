f3ed0bc427ad25438128953779cc2e75e2e94af2
Cheng JIANG <alex_cj96@foxmail.com>
2021-06-30 22:54:29 +0800

    add seperate unit test to reproduce underflow issue

--- pallets/loans/src/tests.rs
+++ pallets/loans/src/tests.rs
@@ -306,17 +306,12 @@ fn repay_borrow_all_works() {
         // Alice deposit 200 DOT as collateral
         assert_ok!(Loans::mint(Origin::signed(ALICE), DOT, million_dollar(200)));
         assert_ok!(Loans::collateral_asset(Origin::signed(ALICE), DOT, true));
-        // Alice borrow 50/1e6 KSM
-        assert_ok!(Loans::borrow(Origin::signed(ALICE), KSM, dollar(50)));
-
-        // Alice borrow 50/1e6 DOT
+        // Alice borrow 50 KSM
         assert_ok!(Loans::borrow(
             Origin::signed(ALICE),
-            DOT,
+            KSM,
             million_dollar(50)
         ));
-
-        run_to_block(150);
 
         // Alice repay all borrow balance
         assert_ok!(Loans::repay_borrow_all(Origin::signed(ALICE), KSM));
@@ -327,13 +322,51 @@ fn repay_borrow_all_works() {
         // KSM borrow balance: borrow - repay = 50 - 50 = 0
         assert_eq!(
             <Runtime as Config>::Currency::free_balance(DOT, &ALICE),
-            million_dollar(850),
+            million_dollar(800),
         );
         assert_eq!(
             Loans::exchange_rate(DOT)
                 .saturating_mul_int(Loans::account_deposits(DOT, ALICE).voucher_balance),
-            200000053852773610000
+            million_dollar(200)
         );
+        let borrow_snapshot = Loans::account_borrows(KSM, ALICE);
+        assert_eq!(borrow_snapshot.principal, 0);
+        assert_eq!(borrow_snapshot.borrow_index, Loans::borrow_index(KSM));
+    })
+}
+
+#[test]
+fn repay_borrow_all_no_underflow() {
+    ExtBuilder::default().build().execute_with(|| {
+        // Alice deposits 200 KSM as collateral
+        assert_ok!(Loans::mint(Origin::signed(ALICE), KSM, million_dollar(200)));
+        assert_ok!(Loans::collateral_asset(Origin::signed(ALICE), KSM, true));
+
+        // Alice borrow only 1/1e6 KSM which is hard to accure total borrows interest in 6 seconds
+        assert_ok!(Loans::borrow(Origin::signed(ALICE), KSM, 10_u128.pow(8)));
+
+        run_to_block(150);
+
+        assert_eq!(Loans::current_borrow_balance(&ALICE, &KSM), Ok(100000056));
+        // FIXME since total_borrows is too small and we accure internal on it every 6 seconds
+        // accure_interest fails every time
+        // as you can see the current borrow balance is not equal to total_borrows anymore
+        assert_eq!(Loans::total_borrows(KSM), 10_u128.pow(8));
+
+        // Alice repay all borrow balance
+        assert_ok!(Loans::repay_borrow_all(Origin::signed(ALICE), KSM));
+
+        assert_eq!(
+            <Runtime as Config>::Currency::free_balance(KSM, &ALICE),
+            million_dollar(800) - 56,
+        );
+
+        assert_eq!(
+            Loans::exchange_rate(DOT)
+                .saturating_mul_int(Loans::account_deposits(KSM, ALICE).voucher_balance),
+            million_dollar(200)
+        );
+
         let borrow_snapshot = Loans::account_borrows(KSM, ALICE);
         assert_eq!(borrow_snapshot.principal, 0);
         assert_eq!(borrow_snapshot.borrow_index, Loans::borrow_index(KSM));
