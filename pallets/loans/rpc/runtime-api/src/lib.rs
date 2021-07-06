e09eecb6cafc16722e2896511a061711338194cd
ayushmishra2005 <ayushmishra2005@gmail.com>
2021-07-06 19:41:22 +0530

    Implemented runtime -api

--- pallets/loans/rpc/runtime-api/src/lib.rs
+++ pallets/loans/rpc/runtime-api/src/lib.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 use codec::Codec;
-use sp_runtime::FixedU128;
+use sp_runtime::{DispatchError, FixedU128};
 
 sp_api::decl_runtime_apis! {
     pub trait LoanApi<AccountId> where
         AccountId: Codec, {
-        fn get_account_liquidity(account: AccountId) -> (FixedU128, FixedU128);
+        fn get_account_liquidity(account: AccountId) -> Result<(FixedU128, FixedU128), DispatchError>;
     }
 }
