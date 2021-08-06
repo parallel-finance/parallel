db47a1128373fa8c279f5155a4579f397f63e32f
Cheng JIANG <alex_cj96@foxmail.com>
2021-08-06 13:50:38 +0800

    add formula

--- runtime/heiko/src/constants.rs
+++ runtime/heiko/src/constants.rs
@@ -74,6 +74,9 @@ pub mod fee {
             // in Kusama, extrinsic base weight (smallest non-zero weight) is mapped to 1/10 CENT:
             // in Statemine, it's mapped to 1/10 of that, or 1/100 CENT
             // in Heiko, it's mapped to 1/100 CENT
+            //
+            // coeff_integer * x^(degree) + coeff_frac * x^(degree)
+            //
             let p = super::currency::CENTS;
             let q = 100 * Balance::from(ExtrinsicBaseWeight::get());
             smallvec![WeightToFeeCoefficient {
--- runtime/vanilla/src/constants.rs
+++ runtime/vanilla/src/constants.rs
@@ -68,7 +68,10 @@ pub mod fee {
         type Balance = Balance;
         fn polynomial() -> WeightToFeeCoefficients<Self::Balance> {
             // in Kusama, extrinsic base weight (smallest non-zero weight) is mapped to 1/10 CENT:
-            // in Statemine, we map to 1/10 of that, or 1/100 CENT
+            // in Vanilla, it's mapped to 1/10 of that, or 1/100 CENT
+            //
+            // coeff_integer * x^(degree) + coeff_frac * x^(degree)
+            //
             let p = super::currency::CENTS;
             let q = 100 * Balance::from(ExtrinsicBaseWeight::get());
             smallvec![WeightToFeeCoefficient {
