97cac809fc4dfc6c46149b03628569c63edbf7cf
Cheng JIANG <alex_cj96@foxmail.com>
2021-08-23 23:10:01 +0800

    bump runtime version

--- runtime/heiko/src/lib.rs
+++ runtime/heiko/src/lib.rs
@@ -126,7 +126,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("heiko"),
     impl_name: create_runtime_str!("heiko"),
     authoring_version: 1,
-    spec_version: 120,
+    spec_version: 130,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
--- runtime/vanilla/src/lib.rs
+++ runtime/vanilla/src/lib.rs
@@ -102,7 +102,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("vanilla"),
     impl_name: create_runtime_str!("vanilla"),
     authoring_version: 1,
-    spec_version: 120,
+    spec_version: 130,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
