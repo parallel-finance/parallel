7929b8250c75907239fc67ed291226f828780383
Alan WANG <alan1995wang@outlook.com>
2021-09-18 09:52:59 +0000

    Resolve comments

--- launch/index.ts
+++ launch/index.ts
@@ -1,12 +1,13 @@
-import { options } from '@parallel-finance/api'
-import { ApiPromise, Keyring, WsProvider } from '@polkadot/api'
-import { exit } from 'process'
+import {options} from '@parallel-finance/api'
+import {ApiPromise, Keyring, WsProvider} from '@polkadot/api'
+import {assets} from './assets.json'
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 async function main() {
+
   const api = await ApiPromise.create(
     options({
       provider: new WsProvider('ws://localhost:9947'),
@@ -16,50 +17,33 @@ async function main() {
   const chainHeight = async () => {
     const {
       block: {
-        header: { number: height },
+        header: {number: height},
       },
     } = await api.rpc.chain.getBlock()
     return height.toNumber()
   }
 
-  do await sleep(1000)
-  while (!(await chainHeight()))
+  do {
+    await sleep(1000)
+    console.log("Wait for block producing")
+  } while (!(await chainHeight()))
 
-  const keyring = new Keyring({ type: 'sr25519', ss58Format: 110 })
+  const keyring = new Keyring({type: 'sr25519', ss58Format: 110})
   const signer = keyring.addFromUri('//Dave')
 
-  let call: any[] = []
-  const assets: [string, string, number, number][] = [
-    ['Kusama', 'KSM', 100, 12],
-    ['Parallel Kusama', 'xKSM', 101, 12],
-    ['Tether Dollar', 'USDT', 102, 12],
-  ]
+  let call = []
 
-  for (const [name, symbol, id, decimal] of assets) {
+  for (const {name, symbol, assetId, decimal, marketOption} of assets) {
     console.log(`Create ${name}(${symbol}) asset.`)
     call.push(
-      api.tx.sudo.sudo(api.tx.assets.forceCreate(id, signer.address, true, 1)),
+      api.tx.sudo.sudo(api.tx.assets.forceCreate(assetId, signer.address, true, 1)),
       api.tx.sudo.sudo(
-        api.tx.assets.forceSetMetadata(id, name, symbol, decimal, false)
+        api.tx.assets.forceSetMetadata(assetId, name, symbol, decimal, false)
       ),
       api.tx.sudo.sudo(
         api.tx.loans.addMarket(
-          id,
-          api.createType('Market', {
-            collateralFactor: 20,
-            reserveFactor: 10,
-            closeFactor: 0,
-            liquidateIncentive: 110,
-            rateModel: api.createType('InterestRateModel', {
-              jumpModel: api.createType('JumpModel', {
-                baseRate: 2,
-                jumpRate: 10,
-                fullRate: 32,
-                jumpUtilization: 80,
-              }),
-            }),
-            state: 'Pending',
-          })
+          assetId,
+          api.createType('Market', marketOption)
         )
       )
     )
