05f538e72a15baed4b38ac798cf6fa97e6e83657
Cheng JIANG <alex_cj96@foxmail.com>
2021-09-22 08:08:59 +0800

    fix Compact<TAssetBalance> type

--- runtime/vanilla/src/lib.rs
+++ runtime/vanilla/src/lib.rs
@@ -27,12 +27,14 @@ use cumulus_primitives_core::ParaId;
 use frame_support::traits::fungible::{
     Inspect as FungibleInspect, Mutate as FungibleMutate, Transfer as FungibleTransfer,
 };
-use frame_support::traits::fungibles::{Inspect, Mutate, Transfer};
-use frame_support::traits::tokens::{DepositConsequence, WithdrawConsequence};
 use frame_support::{
     dispatch::Weight,
     log,
-    traits::{Contains, Everything, IsInVec},
+    traits::{
+        fungibles::{Inspect, Mutate, Transfer},
+        tokens::{DepositConsequence, WithdrawConsequence},
+        Contains, Everything, IsInVec,
+    },
     PalletId,
 };
 use frame_system::{
