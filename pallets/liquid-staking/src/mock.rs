36c22f149297d4ea3dbe5f54a27cfd625ef0fd7d
Cheng JIANG <cocathecafe@gmail.com>
2021-10-18 21:36:10 +0800

    use UpdateOrigin for non-transact call (#748)

--- pallets/liquid-staking/src/mock.rs
+++ pallets/liquid-staking/src/mock.rs
@@ -408,8 +408,8 @@ pub(crate) fn new_test_ext() -> sp_io::TestExternalities {
 
         LiquidStaking::set_liquid_currency(Origin::signed(BOB), XDOT).unwrap();
         LiquidStaking::set_staking_currency(Origin::signed(BOB), DOT).unwrap();
-        LiquidStaking::update_staking_pool_capacity(Origin::signed(ALICE), dot(10000f64)).unwrap();
-        LiquidStaking::update_xcm_fees_compensation(Origin::signed(ALICE), dot(10f64)).unwrap();
+        LiquidStaking::update_staking_pool_capacity(Origin::signed(BOB), dot(10000f64)).unwrap();
+        LiquidStaking::update_xcm_fees_compensation(Origin::signed(BOB), dot(10f64)).unwrap();
     });
 
     ext
@@ -486,8 +486,8 @@ pub fn para_ext(para_id: u32) -> sp_io::TestExternalities {
 
         LiquidStaking::set_liquid_currency(Origin::signed(BOB), XDOT).unwrap();
         LiquidStaking::set_staking_currency(Origin::signed(BOB), DOT).unwrap();
-        LiquidStaking::update_staking_pool_capacity(Origin::signed(ALICE), dot(10000f64)).unwrap();
-        LiquidStaking::update_xcm_fees_compensation(Origin::signed(ALICE), dot(10f64)).unwrap();
+        LiquidStaking::update_staking_pool_capacity(Origin::signed(BOB), dot(10000f64)).unwrap();
+        LiquidStaking::update_xcm_fees_compensation(Origin::signed(BOB), dot(10f64)).unwrap();
     });
 
     ext
