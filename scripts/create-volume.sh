5cf78b889beb58eb22968752e53d116545613f8b
Cheng JIANG <alex_cj96@foxmail.com>
2021-10-25 17:01:06 +0800

    adapt create-volume script for polkadot-v0.9.11

--- scripts/create-volume.sh
+++ scripts/create-volume.sh
@@ -4,10 +4,10 @@ DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 cd $DIR
 
-# curl -fLO https://ksm-rocksdb.polkashots.io/kusama-9217113.RocksDb.7z
-# wget https://ksm-rocksdb.polkashots.io/kusama-9217113.RocksDb.7z
+# curl -fLO https://ksm-rocksdb.polkashots.io/kusama-9801080.RocksDb.7z
+# wget https://ksm-rocksdb.polkashots.io/kusama-9801080.RocksDb.7z
 # sudo apt install p7zip-full
-# 7z x kusama-9217113.RocksDb.7z
+# 7z x kusama-9801080.RocksDb.7z
 
 set -xe
 
@@ -19,7 +19,7 @@ VOLUME="chains"
 docker volume create $VOLUME || true
 
 mountpoint=$(docker volume inspect $VOLUME | jq '.[].Mountpoint' | tr -d '"')
-sudo mkdir -p $mountpoint/$DB_PATH
-sudo mv $SNAPSHOT_PATH $mountpoint/$DB_PATH/db
+sudo mkdir -p $mountpoint/$DB_PATH/db
+sudo mv $SNAPSHOT_PATH $mountpoint/$DB_PATH/db/full
 
 sudo chown -R $(id -un):$(id -gn) $mountpoint
