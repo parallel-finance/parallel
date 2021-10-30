143044b370292b792b471ac745393fb1c5dfdad7
Cheng JIANG <cocathecafe@gmail.com>
2021-10-30 21:12:57 +0800

    Chore/update scripts (#832)
    * fix create-volume script
    
    * make fullnode script supports telemetry display name

--- scripts/create-volume.sh
+++ scripts/create-volume.sh
@@ -4,15 +4,10 @@ DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 cd $DIR
 
-# curl -fLO https://ksm-rocksdb.polkashots.io/kusama-9801080.RocksDb.7z
-# wget https://ksm-rocksdb.polkashots.io/kusama-9801080.RocksDb.7z
-# sudo apt install p7zip-full
-# 7z x kusama-9801080.RocksDb.7z
-
 set -xe
 
 DB_PATH="polkadot/chains/ksmcc3"
-SNAPSHOT_PATH="db"
+SNAPSHOT_PATH="full"
 VOLUME="chains"
 
 # docker volume rm $VOLUME || true
@@ -20,6 +15,7 @@ docker volume create $VOLUME || true
 
 mountpoint=$(docker volume inspect $VOLUME | jq '.[].Mountpoint' | tr -d '"')
 sudo mkdir -p $mountpoint/$DB_PATH/db
-sudo mv $SNAPSHOT_PATH $mountpoint/$DB_PATH/db/full
+sudo rm -fr $mountpoint/$DB_PATH/db/full
+sudo mv $SNAPSHOT_PATH $mountpoint/$DB_PATH/db
 
 sudo chown -R $(id -un):$(id -gn) $mountpoint
