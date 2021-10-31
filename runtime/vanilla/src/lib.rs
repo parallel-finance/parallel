aa420cdb2d12cf7e40cfba64ab9bd35b69ef90a4
Cheng JIANG <cocathecafe@gmail.com>
2021-10-31 13:32:56 +0800

    bump versions to 1.7.2 (#835)
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- runtime/vanilla/src/lib.rs
+++ runtime/vanilla/src/lib.rs
@@ -134,7 +134,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("vanilla"),
     impl_name: create_runtime_str!("vanilla"),
     authoring_version: 1,
-    spec_version: 171,
+    spec_version: 172,
     impl_version: 20,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
