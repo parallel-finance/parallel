dbad5b6c7639786f66fa95acc41975b75dbcb94b
Ayush <>
2021-11-01 08:14:35 +0530

    Fixed compilation error

--- pallets/liquidity_mining/src/lib.rs
+++ pallets/liquidity_mining/src/lib.rs
@@ -18,12 +18,12 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+extern crate alloc;
+
 #[cfg(test)]
 mod mock;
 #[cfg(test)]
 mod tests;
-
-extern crate alloc;
 
 use codec::{Decode, Encode};
 use frame_support::{
