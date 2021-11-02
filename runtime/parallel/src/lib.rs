9b455ed4ce4ff747ee3ac6547902a0c97d88643f
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2021-11-02 15:18:05 +0000

    correct erroneous comment on LocalOriginToLocation (#849)

--- runtime/parallel/src/lib.rs
+++ runtime/parallel/src/lib.rs
@@ -752,7 +752,8 @@ impl pallet_utility::Config for Runtime {
     type WeightInfo = pallet_utility::weights::SubstrateWeight<Runtime>;
 }
 
-/// No local origins on this chain are allowed to dispatch XCM sends/executions.
+/// Local origins on this chain are allowed to dispatch XCM sends/executions. However, we later
+/// block this via `ExecuteXcmOrigin`.
 pub type LocalOriginToLocation = SignedToAccountId32<Origin, AccountId, RelayNetwork>;
 
 /// The means for routing XCM messages which are not for local execution into the right message
