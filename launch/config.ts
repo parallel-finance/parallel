92a030a862942a033a54cdca544b2971688ddf42
Cheng JIANG <cocathecafe@gmail.com>
2021-11-12 12:26:10 +0800

    Launch scripts for crowdloans (#880)
    * add basic crowdloan derivatives
    
    * add crowdloans support for launch scripts
    
    * fix ts build
    
    * add charlie to be parachain collator

--- launch/config.ts
+++ launch/config.ts
@@ -27,7 +27,7 @@ const config = {
       balances: []
     },
     {
-      name: 'Parallel Kusama',
+      name: 'Parallel Staking Kusama',
       symbol: 'XKSM',
       assetId: 1000,
       decimal: 12,
@@ -73,6 +73,38 @@ const config = {
         ptokenId: 2102
       },
       balances: [['5HHMY7e8UAqR5ZaHGaQnRW5EDR8dP7QpAyjeBu6V7vdXxxbf', '100000000000000000000']]
+    },
+    {
+      name: 'Parallel Crowdloans Kusama',
+      symbol: 'CKSM',
+      assetId: 4000,
+      decimal: 12,
+      marketOption: {
+        closeFactor: 50e4,
+        collateralFactor: 50e4,
+        reserveFactor: 15e4,
+        cap: '100000000000000000',
+        liquidateIncentive: '1100000000000000000',
+        rateModel: {
+          jumpModel: {
+            baseRate: '20000000000000000',
+            jumpRate: '100000000000000000',
+            fullRate: '320000000000000000',
+            jumpUtilization: 8e5
+          }
+        },
+        state: 'Pending',
+        ptokenId: 3100
+      },
+      balances: []
+    }
+  ],
+  crowdloans: [
+    {
+      paraId: 2000,
+      image: 'parallelfinance/polkadot-collator:v0.9.12',
+      chain: 'shell',
+      ctokenId: 4000
     }
   ]
 }
--- launch/index.ts
+++ launch/index.ts
@@ -1,13 +1,28 @@
+import fs from 'fs'
+import shell from 'shelljs'
 import { options } from '@parallel-finance/api'
 import { ApiPromise, Keyring, WsProvider } from '@polkadot/api'
+import dotenv from 'dotenv'
 import config from './config'
 import '@parallel-finance/types'
+
+dotenv.config()
 
 function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-async function main() {
+function exec(cmd: string) {
+  console.log(`$ ${cmd}`)
+  const res = shell.exec(cmd, { silent: true })
+  if (res.code !== 0) {
+    console.error('Error: Command failed with code', res.code)
+    console.log(res)
+  }
+  return res
+}
+
+async function para() {
   const api = await ApiPromise.create(
     options({
       types: {
@@ -26,7 +41,7 @@ async function main() {
     return height.toNumber()
   }
 
-  console.log('Wait for block producing')
+  console.log('Wait for parachain to produce blocks')
   do await sleep(1000)
   while (!(await chainHeight()))
 
@@ -46,6 +61,10 @@ async function main() {
     call.push(...balances.map(([account, amount]) => api.tx.assets.mint(assetId, account, amount)))
   }
 
+  for (const { paraId, image, chain, ctokenId } of config.crowdloans) {
+    call.push(api.tx.sudo.sudo(api.tx.crowdloans.createVault(paraId, ctokenId, 'XCM')))
+  }
+
   call.push(
     api.tx.sudo.sudo(api.tx.liquidStaking.setLiquidCurrency(config.liquidAsset)),
     api.tx.sudo.sudo(api.tx.liquidStaking.setStakingCurrency(config.stakingAsset)),
@@ -54,10 +73,66 @@ async function main() {
     api.tx.sudo.sudo(api.tx.crowdloans.updateXcmFeesCompensation('50000000000'))
   )
 
-  console.log('Submit batches.')
+  console.log('Submit parachain batches.')
   const nonce = await api.rpc.system.accountNextIndex(signer.address)
   await api.tx.utility.batchAll(call).signAndSend(signer, { nonce })
-  process.exit(0)
 }
 
-main()
+async function relay() {
+  const api = await ApiPromise.create({
+    provider: new WsProvider('ws://localhost:9944')
+  })
+
+  const chainHeight = async () => {
+    const {
+      block: {
+        header: { number: height }
+      }
+    } = await api.rpc.chain.getBlock()
+    return height.toNumber()
+  }
+
+  console.log('Wait for relaychain to produce blocks')
+  do await sleep(1000)
+  while (!(await chainHeight()))
+
+  const keyring = new Keyring({ type: 'sr25519', ss58Format: 2 })
+  const signer = keyring.addFromUri(`${process.env.RELAY_CHAIN_SUDO_KEY || ''}`)
+
+  let call = []
+
+  for (const { paraId, image, chain, ctokenId } of config.crowdloans) {
+    const state = exec(
+      `docker run --rm ${image} export-genesis-state --chain ${chain} --parachain-id ${paraId}`
+    ).stdout.trim()
+    const wasm = exec(`docker run --rm ${image} export-genesis-wasm --chain ${chain}`).stdout.trim()
+    call.push(
+      api.tx.sudo.sudo(api.tx.registrar.forceRegister(signer.address, 100, paraId, state, wasm))
+    )
+  }
+
+  console.log('Submit relaychain batches.')
+  await api.tx.utility
+    .batchAll(call)
+    .signAndSend(signer, { nonce: await api.rpc.system.accountNextIndex(signer.address) })
+  console.log('Wait parathread to be onboarded.')
+  await sleep(360000)
+
+  const height = await chainHeight()
+
+  call.push(api.tx.sudo.sudo(api.tx.auctions.newAuction(1000000, 0)))
+  call.push(
+    ...config.crowdloans.map(({ paraId }) =>
+      api.tx.crowdloan.create(paraId, '1000000000000000000', 0, 7, height + 500000, null)
+    )
+  )
+
+  await api.tx.utility
+    .batchAll(call)
+    .signAndSend(signer, { nonce: await api.rpc.system.accountNextIndex(signer.address) })
+}
+
+relay()
+  .then(para)
+  .then(() => process.exit(0))
+  .catch(() => process.exit(1))
