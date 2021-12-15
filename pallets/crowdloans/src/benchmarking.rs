34188951c3d7b42dc65ab830fa54c6b7d6ba1a50
drbh <david.richard.holtz@gmail.com>
2021-12-14 20:03:10 -0500

    clippy fix

--- pallets/crowdloans/src/benchmarking.rs
+++ pallets/crowdloans/src/benchmarking.rs
@@ -8,7 +8,7 @@ use crate::Pallet as Crowdloans;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_support::{assert_ok, pallet_prelude::*, traits::fungibles::Mutate};
 use frame_system::{self, RawOrigin as SystemOrigin};
-use primitives::{ump::*, Balance, BlockNumber, CurrencyId, ParaId};
+use primitives::{ump::*, Balance, CurrencyId, ParaId};
 use sp_runtime::traits::{StaticLookup, Zero};
 use sp_std::prelude::*;
 
