faa5cf596223e8db7e1d7f379e41752639b6dcb3
Cheng JIANG <alex_cj96@foxmail.com>
2021-12-28 09:11:34 +0800

    bump versions
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- runtime/vanilla/src/lib.rs
+++ runtime/vanilla/src/lib.rs
@@ -136,10 +136,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("vanilla"),
     impl_name: create_runtime_str!("vanilla"),
     authoring_version: 1,
-    spec_version: 175,
-    impl_version: 20,
+    spec_version: 176,
+    impl_version: 21,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 4,
+    transaction_version: 5,
 };
 
 // 1 in 4 blocks (on average, not counting collisions) will be primary babe blocks.
