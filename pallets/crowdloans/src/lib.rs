47fe8b1cd9ef9d4685f452b96db89b091b37afbd
gayan <kalanamith@gmail.com>
2022-01-18 11:18:35 +1300

    added fix point

--- pallets/crowdloans/src/lib.rs
+++ pallets/crowdloans/src/lib.rs
@@ -996,6 +996,7 @@ pub mod pallet {
                 Error::<T>::IncorrectVaultPhase
             );
 
+            // TODO: Fix iterate an enum or may be convert it to iterator
             for child_storage_kind in ChildStorageKind {
                 let contribution = Self::contribution_iterator(
                     vault.trie_index, child_storage_kind
