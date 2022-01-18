363ff91d7ced4d2eb42a46548c1703c26c67afef
Ayush <>
2022-01-18 14:10:18 +0530

    Addressed PR comments

--- pallets/amm/src/lib.rs
+++ pallets/amm/src/lib.rs
@@ -313,8 +313,9 @@ pub mod pallet {
                 base_asset,
                 quote_asset,
                 |pool_liquidity_amount| -> DispatchResult {
-                    let mut liquidity_amount =
-                        pool_liquidity_amount.ok_or(Error::<T, I>::PoolDoesNotExist)?;
+                    let mut liquidity_amount = pool_liquidity_amount
+                        .as_mut()
+                        .ok_or(Error::<T, I>::PoolDoesNotExist)?;
 
                     let total_ownership = T::Assets::total_issuance(liquidity_amount.pool_assets);
                     ensure!(
@@ -342,11 +343,10 @@ pub mod pallet {
                         .checked_sub(quote_amount)
                         .ok_or(ArithmeticError::Underflow)?;
 
-                    *pool_liquidity_amount = Some(liquidity_amount);
                     LiquidityProviders::<T, I>::try_mutate(
                         (&who, &base_asset, &quote_asset),
                         |pool_liquidity_amount| -> DispatchResult {
-                            if let Some(liquidity_amount) = pool_liquidity_amount {
+                            if let Some(liquidity_amount) = pool_liquidity_amount.as_mut() {
                                 liquidity_amount.base_amount = liquidity_amount
                                     .base_amount
                                     .checked_sub(base_amount)
@@ -355,7 +355,6 @@ pub mod pallet {
                                     .quote_amount
                                     .checked_sub(quote_amount)
                                     .ok_or(ArithmeticError::Underflow)?;
-                                *pool_liquidity_amount = Some(*liquidity_amount);
                             }
                             Ok(())
                         },
@@ -506,8 +505,9 @@ impl<T: Config<I>, I: 'static> primitives::AMM<T, AssetIdOf<T, I>, BalanceOf<T, 
             &quote_asset,
             |pool_liquidity_amount| -> Result<BalanceOf<T, I>, DispatchError> {
                 // 1. If the pool we want to trade does not exist in the current instance, error
-                let mut liquidity_amount =
-                    pool_liquidity_amount.ok_or(Error::<T, I>::PoolDoesNotExist)?;
+                let mut liquidity_amount = pool_liquidity_amount
+                    .as_mut()
+                    .ok_or(Error::<T, I>::PoolDoesNotExist)?;
 
                 // supply_in == liquidity_amount.base_amount unless inverted
                 let (supply_in, supply_out) = if is_inverted {
@@ -579,7 +579,6 @@ impl<T: Config<I>, I: 'static> primitives::AMM<T, AssetIdOf<T, I>, BalanceOf<T, 
                         .checked_sub(amount_out)
                         .ok_or(ArithmeticError::Underflow)?;
                 }
-                *pool_liquidity_amount = Some(liquidity_amount);
 
                 // 6. Wire amount_in of the input token (identified by pair.0) from who to PalletId
                 T::Assets::transfer(
