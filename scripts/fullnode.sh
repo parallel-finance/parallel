0f1afcb91166a6fb7d895e4aa75ede05b2d141b6
Cheng JIANG <cocathecafe@gmail.com>
2022-01-26 00:51:51 -0600

    update scripts to support other chains (#1226)
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- scripts/fullnode.sh
+++ scripts/fullnode.sh
@@ -18,6 +18,8 @@ PARA_CHAIN="${2:-heiko}"
 RELAY_CHAIN="${3:-kusama}"
 VOLUME="chains"
 NODE_NAME="$1"
+DOCKER_IMAGE="parallelfinance/parallel:v1.7.7"
+BASE_PATH="/data"
 
 if [ $# -lt 1 ]; then
   echo "help: ./fullnode.sh <NODE_NAME>" && exit 1
@@ -38,9 +40,9 @@ docker run --restart=always --name $PARA_CHAIN-fullnode \
   -p $RELAY_WS_PORT:$RELAY_WS_PORT \
   -p $RELAY_RPC_PORT:$RELAY_RPC_PORT \
   -p $RELAY_P2P_PORT:$RELAY_P2P_PORT \
-  -v "$VOLUME:/data" \
-  parallelfinance/parallel:v1.7.7 \
-    -d /data \
+  -v "$VOLUME:$BASE_PATH" \
+  $DOCKER_IMAGE \
+    -d $BASE_PATH \
     --chain=$PARA_CHAIN \
     --ws-port=$PARA_WS_PORT \
     --rpc-port=$PARA_RPC_PORT \
