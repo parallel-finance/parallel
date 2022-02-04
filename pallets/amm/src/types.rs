4c3aed12d37cb4cee20a08a9fd498847a4dfbdf4
Ayush <81686485+ashi20192020ayush@users.noreply.github.com>
2022-02-04 06:41:32 +0530

    Implemented TWAP oracle (#1248)
    * Implemented TWAP oracle
    
    * add test and fix time_elapsed logic
    
    Co-authored-by: Ayush <>
    Co-authored-by: drbh <david.richard.holtz@gmail.com>

--- pallets/amm/src/types.rs
+++ pallets/amm/src/types.rs
@@ -5,20 +5,26 @@ use sp_runtime::{traits::Zero, RuntimeDebug};
 
 #[derive(Encode, Decode, Eq, PartialEq, Copy, Clone, RuntimeDebug, PartialOrd, Ord, TypeInfo)]
 #[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
-pub struct Pool<CurrencyId, Balance> {
+pub struct Pool<CurrencyId, Balance, BlockNumber> {
     pub base_amount: Balance,
     pub quote_amount: Balance,
     pub root_k_last: Balance,
     pub lp_token_id: CurrencyId,
+    pub block_timestamp_last: BlockNumber,
+    pub price_0_cumulative_last: Balance,
+    pub price_1_cumulative_last: Balance,
 }
 
-impl<CurrencyId, Balance: BalanceT> Pool<CurrencyId, Balance> {
+impl<CurrencyId, Balance: BalanceT, BlockNumber: BalanceT> Pool<CurrencyId, Balance, BlockNumber> {
     pub fn new(lp_token_id: CurrencyId) -> Self {
         Self {
             base_amount: Zero::zero(),
             quote_amount: Zero::zero(),
             root_k_last: Zero::zero(),
             lp_token_id,
+            block_timestamp_last: Zero::zero(),
+            price_0_cumulative_last: Zero::zero(),
+            price_1_cumulative_last: Zero::zero(),
         }
     }
 
