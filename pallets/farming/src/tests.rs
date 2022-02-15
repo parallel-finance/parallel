b16f979d79343ffa447b471829c5d7b3ba3889b1
Yang Zhou <deedecho@gmail.com>
2022-02-15 09:23:42 +0800

    rename liquidity-mining pallet to farming

--- pallets/farming/src/tests.rs
+++ pallets/farming/src/tests.rs
@@ -11,7 +11,7 @@ fn create_pool_should_work() {
         let initial_balance = Assets::balance(DOT, BOB);
 
         assert_eq!(Assets::balance(DOT, BOB), initial_balance);
-        assert_ok!(LiquidityMining::create(
+        assert_ok!(Farming::create(
             RawOrigin::Signed(ALICE).into(),
             DOT,
             BOB,
@@ -30,7 +30,7 @@ fn create_pool_should_work() {
 #[test]
 fn create_pool_should_not_work_if_pool_already_exists() {
     new_test_ext().execute_with(|| {
-        assert_ok!(LiquidityMining::create(
+        assert_ok!(Farming::create(
             RawOrigin::Signed(ALICE).into(),
             DOT,
             BOB,
@@ -41,7 +41,7 @@ fn create_pool_should_not_work_if_pool_already_exists() {
         ));
 
         assert_noop!(
-            LiquidityMining::create(
+            Farming::create(
                 RawOrigin::Signed(ALICE).into(),
                 DOT,
                 BOB,
@@ -59,7 +59,7 @@ fn create_pool_should_not_work_if_pool_already_exists() {
 fn create_pool_should_not_work_if_not_a_newly_asset() {
     new_test_ext().execute_with(|| {
         assert_noop!(
-            LiquidityMining::create(
+            Farming::create(
                 RawOrigin::Signed(ALICE).into(),
                 DOT,
                 BOB,
@@ -79,7 +79,7 @@ fn create_pool_should_not_work_if_endblock_smaller_than_startblock() {
         let (start_block, end_block) = (6, 2);
 
         assert_noop!(
-            LiquidityMining::create(
+            Farming::create(
                 RawOrigin::Signed(ALICE).into(),
                 DOT,
                 BOB,
@@ -99,7 +99,7 @@ fn deposit_should_work() {
         let initial_balance = Assets::balance(DOT, BOB);
 
         assert_eq!(Assets::balance(DOT, BOB), initial_balance);
-        assert_ok!(LiquidityMining::create(
+        assert_ok!(Farming::create(
             RawOrigin::Signed(ALICE).into(),
             DOT,
             BOB,
@@ -111,11 +111,7 @@ fn deposit_should_work() {
 
         assert_eq!(Assets::balance(DOT, BOB), initial_balance - 3000);
 
-        assert_ok!(LiquidityMining::deposit(
-            RawOrigin::Signed(BOB).into(),
-            DOT,
-            1000
-        ));
+        assert_ok!(Farming::deposit(RawOrigin::Signed(BOB).into(), DOT, 1000));
 
         assert_eq!(Assets::balance(DOT, BOB), initial_balance - 3000 - 1000);
     })
@@ -125,7 +121,7 @@ fn deposit_should_work() {
 fn deposit_should_not_work_if_amount_is_zero() {
     new_test_ext().execute_with(|| {
         assert_noop!(
-            LiquidityMining::deposit(RawOrigin::Signed(BOB).into(), DOT, 0),
+            Farming::deposit(RawOrigin::Signed(BOB).into(), DOT, 0),
             Error::<Test>::NotAValidAmount
         );
     })
@@ -135,7 +131,7 @@ fn deposit_should_not_work_if_amount_is_zero() {
 fn deposit_should_not_work_if_pool_does_not_exist() {
     new_test_ext().execute_with(|| {
         assert_noop!(
-            LiquidityMining::deposit(RawOrigin::Signed(BOB).into(), DOT, 1000),
+            Farming::deposit(RawOrigin::Signed(BOB).into(), DOT, 1000),
             Error::<Test>::PoolDoesNotExist
         );
     })
@@ -145,7 +141,7 @@ fn deposit_should_not_work_if_pool_does_not_exist() {
 fn deposit_should_not_work_if_not_a_valid_duration() {
     new_test_ext().execute_with(|| {
         let (start_block, end_block) = (3, 100);
-        assert_ok!(LiquidityMining::create(
+        assert_ok!(Farming::create(
             RawOrigin::Signed(ALICE).into(),
             DOT,
             BOB,
@@ -158,14 +154,14 @@ fn deposit_should_not_work_if_not_a_valid_duration() {
         // current block number is smaller than pool.start
         System::set_block_number(start_block - 1);
         assert_noop!(
-            LiquidityMining::deposit(RawOrigin::Signed(BOB).into(), DOT, 1000),
+            Farming::deposit(RawOrigin::Signed(BOB).into(), DOT, 1000),
             Error::<Test>::NotAValidDuration
         );
 
         // current block number is bigger than pool.end
         System::set_block_number(end_block + 1);
         assert_noop!(
-            LiquidityMining::deposit(RawOrigin::Signed(BOB).into(), DOT, 1000),
+            Farming::deposit(RawOrigin::Signed(BOB).into(), DOT, 1000),
             Error::<Test>::NotAValidDuration
         );
     })
@@ -177,7 +173,7 @@ fn withdraw_should_work() {
         let initial_balance = Assets::balance(DOT, BOB);
 
         assert_eq!(Assets::balance(DOT, BOB), initial_balance);
-        assert_ok!(LiquidityMining::create(
+        assert_ok!(Farming::create(
             RawOrigin::Signed(ALICE).into(),
             DOT,
             BOB,
@@ -189,17 +185,9 @@ fn withdraw_should_work() {
 
         assert_eq!(Assets::balance(DOT, BOB), initial_balance - 3000);
 
-        assert_ok!(LiquidityMining::deposit(
-            RawOrigin::Signed(BOB).into(),
-            DOT,
-            1000
-        ));
+        assert_ok!(Farming::deposit(RawOrigin::Signed(BOB).into(), DOT, 1000));
 
-        assert_ok!(LiquidityMining::withdraw(
-            RawOrigin::Signed(BOB).into(),
-            DOT,
-            1000
-        ));
+        assert_ok!(Farming::withdraw(RawOrigin::Signed(BOB).into(), DOT, 1000));
 
         assert_eq!(Assets::balance(DOT, BOB), initial_balance - 3000);
     })
@@ -209,7 +197,7 @@ fn withdraw_should_work() {
 fn withdraw_should_not_work_if_amount_is_zero() {
     new_test_ext().execute_with(|| {
         assert_noop!(
-            LiquidityMining::withdraw(RawOrigin::Signed(BOB).into(), DOT, 0),
+            Farming::withdraw(RawOrigin::Signed(BOB).into(), DOT, 0),
             Error::<Test>::NotAValidAmount
         );
     })
@@ -219,7 +207,7 @@ fn withdraw_should_not_work_if_amount_is_zero() {
 fn withdraw_should_not_work_if_pool_does_not_exist() {
     new_test_ext().execute_with(|| {
         assert_noop!(
-            LiquidityMining::withdraw(RawOrigin::Signed(BOB).into(), DOT, 1000),
+            Farming::withdraw(RawOrigin::Signed(BOB).into(), DOT, 1000),
             Error::<Test>::PoolDoesNotExist
         );
     })
@@ -229,15 +217,15 @@ fn withdraw_should_not_work_if_pool_does_not_exist() {
 fn create_pool_account_id_should_work() {
     new_test_ext().execute_with(|| {
         assert_eq!(
-            LiquidityMining::pool_account_id(tokens::DOT).unwrap(),
+            Farming::pool_account_id(tokens::DOT).unwrap(),
             AccountId(5650623433380315385)
         );
         assert_eq!(
-            LiquidityMining::pool_account_id(tokens::XDOT).unwrap(),
+            Farming::pool_account_id(tokens::XDOT).unwrap(),
             AccountId(17971758411142122835)
         );
         assert_eq!(
-            LiquidityMining::pool_account_id(tokens::PARA).unwrap(),
+            Farming::pool_account_id(tokens::PARA).unwrap(),
             AccountId(12297710138430822110)
         );
     })
