c5e71ab18ce4bff46e2db55a6d5d6d7fd4936acb
Cheng JIANG <alex_cj96@foxmail.com>
2022-03-08 22:12:22 +0800

    fix gift amount of relaychain asset
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- runtime/parallel/src/lib.rs
+++ runtime/parallel/src/lib.rs
@@ -964,7 +964,7 @@ impl BalanceConversion<Balance, CurrencyId, Balance> for GiftConvert {
 
         let default_gift_amount = 125 * DOLLARS / 100; // 1.25PARA
         Ok(match asset_id {
-            NATIVE_ASSET_ID if balance >= 5 * 10_u128.pow(decimal.into()) => default_gift_amount,
+            DOT if balance >= 5 * 10_u128.pow(decimal.into()) => default_gift_amount,
             EUSDT | EUSDC if balance >= 300 * 10_u128.pow(decimal.into()) => default_gift_amount,
             _ => Zero::zero(),
         })
