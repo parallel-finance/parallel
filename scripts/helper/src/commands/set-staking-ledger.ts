456a82e67a9e1d8d26fea8195cc5ef7d744b3ca6
Cheng JIANG <alex_cj96@foxmail.com>
2022-03-25 23:50:43 +0800

    rename command
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- scripts/helper/src/commands/set-staking-ledger.ts
+++ scripts/helper/src/commands/set-staking-ledger.ts
@@ -10,33 +10,34 @@ import { Option } from '@polkadot/types'
 export default function ({ createCommand }: CreateCommandParameters): Command {
   return createCommand('Fetch Relaychain Ledger and update to Parachain')
     .option('-r, --relay-ws [url]', 'the relaychain API endpoint', {
-      default: 'ws://127.0.0.1:9944'
+      default: 'wss://cj-dev-relay-rpc.parallel.fi'
     })
     .option('-p, --para-ws [url]', 'the parachain API endpoint', {
-      default: 'ws://127.0.0.1:9948'
+      default: 'wss://cj-dev-rpc.parallel.fi'
     })
     .action(async actionParameters => {
       const {
+        logger,
         options: { relayWs, paraWs }
       } = actionParameters
       const relayApi = await getRelayApi(relayWs.toString())
       const api = await getApi(paraWs.toString())
 
-      const block_hash = await api.rpc.chain.getBlockHash()
-      console.log('parachain block hash: ' + block_hash.toString())
+      const blockHash = await api.rpc.chain.getBlockHash()
+      logger.info('parachain block hash: ' + blockHash.toString())
 
-      const validationDataOp = (await api.query.liquidStaking.validationData.at(
-        block_hash.toString()
+      const maybeValidationData = (await api.query.liquidStaking.validationData.at(
+        blockHash.toString()
       )) as unknown as Option<PersistedValidationData>
-      const validationData = validationDataOp.unwrap()
+      const validationData = maybeValidationData.unwrap()
 
       // const relayParentNumber = validationData.relayParentNumber
-      console.log(JSON.stringify(validationData, null, 4))
+      logger.info(JSON.stringify(validationData, null, 4))
       const relayBlockHash = await relayApi.rpc.chain.getBlockHash()
-      console.log('relaychain block hash: ' + relayBlockHash.toString())
+      logger.info('relaychain block hash: ' + relayBlockHash.toString())
 
-      const staking_address = 'EgPaU9rG6nxwQ4k1HTDzUS9acjig14r1hBM3V9JERrgviHU'
-      const accountBytes = decodeAddress(staking_address)
+      const controllerAddress = 'EgPaU9rG6nxwQ4k1HTDzUS9acjig14r1hBM3V9JERrgviHU'
+      const accountBytes = decodeAddress(controllerAddress)
       const storageKey = u8aToHex(
         new Uint8Array([
           ...xxhashAsU8a('Staking', 128),
@@ -49,29 +50,35 @@ export default function ({ createCommand }: CreateCommandParameters): Command {
         relayBlockHash
       )) as ReadProof
 
-      // console.log(storageKey)
-      // console.log(JSON.stringify(proof, null, 4))
+      // logger.info(storageKey)
+      // logger.info(JSON.stringify(proof, null, 4))
       const proof_bytes = proof.proof
 
-      const ledgerOp = (await relayApi.query.staking.ledger(
-        staking_address
+      const mnaybeLedger = (await relayApi.query.staking.ledger(
+        controllerAddress
       )) as unknown as Option<StakingLedger>
-      // console.log(JSON.stringify(ledgerOp, null, 4))
-      const ledger = ledgerOp.unwrap()
+      // logger.info(JSON.stringify(ledgerOp, null, 4))
+      const ledger = mnaybeLedger.unwrap()
 
       const keyring = new Keyring({ type: 'sr25519' })
       const signer = keyring.addFromUri(`${process.env.PARA_CHAIN_SUDO_KEY || '//Dave'}`)
       await api.tx.sudo
-        .sudo(api.tx.liquidStaking.setStakingLedger(0, ledger, proof_bytes))
+        .sudo(
+          api.tx.liquidStaking.setStakingLedger(
+            api.consts.liquidStaking.derivativeIndex.toString(),
+            ledger,
+            proof_bytes
+          )
+        )
         .signAndSend(signer, ({ events = [], status }) => {
           if (status.isInBlock) {
-            console.log('Successful with hash ' + status.asInBlock.toHex())
+            logger.info('Successful with hash ' + status.asInBlock.toHex())
           } else {
-            console.log('Status of transfer: ' + status.type)
+            logger.info('Status of transfer: ' + status.type)
           }
 
           events.forEach(({ phase, event: { data, method, section } }) => {
-            console.log(phase.toString() + ' : ' + section + '.' + method + ' ' + data.toString())
+            logger.info(phase.toString() + ' : ' + section + '.' + method + ' ' + data.toString())
           })
         })
 
