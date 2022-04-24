0d22e06a956b405119e06880b8d9ea8095e31669
MrPai <1164934857@qq.com>
2022-04-24 16:05:02 +0800

    Ls/improve distribute (#1646)
    * update algo
    
    * update ledger.total and ledger.active
    
    * rename to unbonding_amounts
    
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>
    
    Co-authored-by: Cheng JIANG <alex_cj96@foxmail.com>

--- pallets/liquid-staking/src/distribution.rs
+++ pallets/liquid-staking/src/distribution.rs
@@ -7,19 +7,19 @@ use sp_std::vec::Vec;
 pub struct AverageDistribution;
 impl<Balance: BalanceT + FixedPointOperand> DistributionStrategy<Balance> for AverageDistribution {
     fn get_bond_distributions(
-        bonded_amounts: Vec<(DerivativeIndex, Balance)>,
+        total_bonded_amounts: Vec<(DerivativeIndex, Balance)>,
         input: Balance,
         cap: Balance,
         min_nominator_bond: Balance,
     ) -> Vec<(DerivativeIndex, Balance)> {
-        let length = TryInto::<Balance>::try_into(bonded_amounts.len()).unwrap_or_default();
+        let length = TryInto::<Balance>::try_into(total_bonded_amounts.len()).unwrap_or_default();
         if length.is_zero() {
             return Default::default();
         }
 
         let mut distributions: Vec<(DerivativeIndex, Balance)> = vec![];
         let amount = input.checked_div(&length).unwrap_or_default();
-        for (index, bonded) in bonded_amounts.into_iter() {
+        for (index, bonded) in total_bonded_amounts.into_iter() {
             if amount.saturating_add(bonded) < min_nominator_bond {
                 continue;
             }
@@ -34,18 +34,18 @@ impl<Balance: BalanceT + FixedPointOperand> DistributionStrategy<Balance> for Av
     }
 
     fn get_unbond_distributions(
-        bonded_amounts: Vec<(DerivativeIndex, Balance)>,
+        active_bonded_amounts: Vec<(DerivativeIndex, Balance)>,
         input: Balance,
         min_nominator_bond: Balance,
     ) -> Vec<(DerivativeIndex, Balance)> {
-        let length = TryInto::<Balance>::try_into(bonded_amounts.len()).unwrap_or_default();
+        let length = TryInto::<Balance>::try_into(active_bonded_amounts.len()).unwrap_or_default();
         if length.is_zero() {
             return Default::default();
         }
 
         let mut distributions: Vec<(DerivativeIndex, Balance)> = vec![];
         let amount = input.checked_div(&length).unwrap_or_default();
-        for (index, bonded) in bonded_amounts.into_iter() {
+        for (index, bonded) in active_bonded_amounts.into_iter() {
             if bonded.saturating_sub(amount) < min_nominator_bond {
                 continue;
             }
@@ -59,19 +59,18 @@ impl<Balance: BalanceT + FixedPointOperand> DistributionStrategy<Balance> for Av
     }
 
     fn get_rebond_distributions(
-        bonded_amounts: Vec<(DerivativeIndex, Balance)>,
+        unbonding_amounts: Vec<(DerivativeIndex, Balance)>,
         input: Balance,
-        cap: Balance,
     ) -> Vec<(DerivativeIndex, Balance)> {
-        let length = TryInto::<Balance>::try_into(bonded_amounts.len()).unwrap_or_default();
+        let length = TryInto::<Balance>::try_into(unbonding_amounts.len()).unwrap_or_default();
         if length.is_zero() {
             return Default::default();
         }
 
         let mut distributions: Vec<(DerivativeIndex, Balance)> = vec![];
-        let amount = input.checked_div(&length).unwrap_or_default();
-        for (index, bonded) in bonded_amounts.into_iter() {
-            let amount = amount.min(cap.saturating_sub(bonded));
+        let mut amount = input.checked_div(&length).unwrap_or_default();
+        for (index, unlocking) in unbonding_amounts.into_iter() {
+            amount = amount.min(unlocking);
             if amount.is_zero() {
                 continue;
             }
@@ -85,18 +84,18 @@ impl<Balance: BalanceT + FixedPointOperand> DistributionStrategy<Balance> for Av
 pub struct MaxMinDistribution;
 impl<Balance: BalanceT + FixedPointOperand> DistributionStrategy<Balance> for MaxMinDistribution {
     fn get_bond_distributions(
-        mut bonded_amounts: Vec<(DerivativeIndex, Balance)>,
+        mut total_bonded_amounts: Vec<(DerivativeIndex, Balance)>,
         input: Balance,
         cap: Balance,
         min_nominator_bond: Balance,
     ) -> Vec<(DerivativeIndex, Balance)> {
         // ascending sequence
-        bonded_amounts.sort_by(|a, b| a.1.cmp(&b.1));
+        total_bonded_amounts.sort_by(|a, b| a.1.cmp(&b.1));
 
         let mut distributions: Vec<(DerivativeIndex, Balance)> = vec![];
         let mut remain = input;
 
-        for (index, bonded) in bonded_amounts.into_iter() {
+        for (index, bonded) in total_bonded_amounts.into_iter() {
             if remain.is_zero() {
                 break;
             }
@@ -119,17 +118,17 @@ impl<Balance: BalanceT + FixedPointOperand> DistributionStrategy<Balance> for Ma
     }
 
     fn get_unbond_distributions(
-        mut bonded_amounts: Vec<(DerivativeIndex, Balance)>,
+        mut active_bonded_amounts: Vec<(DerivativeIndex, Balance)>,
         input: Balance,
         min_nominator_bond: Balance,
     ) -> Vec<(DerivativeIndex, Balance)> {
         // descending sequence
-        bonded_amounts.sort_by(|a, b| b.1.cmp(&a.1));
+        active_bonded_amounts.sort_by(|a, b| b.1.cmp(&a.1));
 
         let mut distributions: Vec<(DerivativeIndex, Balance)> = vec![];
         let mut remain = input;
 
-        for (index, bonded) in bonded_amounts.into_iter() {
+        for (index, bonded) in active_bonded_amounts.into_iter() {
             if remain.is_zero() {
                 break;
             }
@@ -145,25 +144,22 @@ impl<Balance: BalanceT + FixedPointOperand> DistributionStrategy<Balance> for Ma
     }
 
     fn get_rebond_distributions(
-        mut bonded_amounts: Vec<(DerivativeIndex, Balance)>,
+        mut unbonding_amounts: Vec<(DerivativeIndex, Balance)>,
         input: Balance,
-        cap: Balance,
     ) -> Vec<(DerivativeIndex, Balance)> {
-        // ascending sequence
-        bonded_amounts.sort_by(|a, b| a.1.cmp(&b.1));
+        // descending sequence
+        unbonding_amounts.sort_by(|a, b| b.1.cmp(&a.1));
 
         let mut distributions: Vec<(DerivativeIndex, Balance)> = vec![];
         let mut remain = input;
 
-        for (index, bonded) in bonded_amounts.into_iter() {
+        for (index, unlocking) in unbonding_amounts.into_iter() {
             if remain.is_zero() {
                 break;
             }
-            let amount = cap.saturating_sub(bonded).min(remain);
+            let amount = remain.min(unlocking);
             if amount.is_zero() {
-                // `bonding_amounts` is an ascending sequence
-                // if occurs an item that exceed the cap, the items after this one must all be exceeded
-                break;
+                continue;
             }
             distributions.push((index, amount));
             remain = remain.saturating_sub(amount);
