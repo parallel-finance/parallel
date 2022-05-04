34ba0435c729625a7eca2611c59da8c3a7023fe7
Cheng JIANG <cocathecafe@gmail.com>
2022-05-04 12:41:13 +0800

    fix exchange rate formula (#1703)
    * fix exchange rate formula
    
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>
    
    * rename
    
    Signed-off-by: Cheng JIANG <alex_cj96@foxmail.com>

--- pallets/liquid-staking/src/lib.rs
+++ pallets/liquid-staking/src/lib.rs
@@ -961,6 +961,12 @@ pub mod pallet {
                 .fold(Zero::zero(), |acc, ledger| acc.saturating_add(ledger.total))
         }
 
+        fn get_total_active_bonded() -> BalanceOf<T> {
+            StakingLedgers::<T>::iter_values().fold(Zero::zero(), |acc, ledger| {
+                acc.saturating_add(ledger.active)
+            })
+        }
+
         fn get_market_cap() -> BalanceOf<T> {
             Self::staking_ledger_cap()
                 .saturating_mul(T::DerivativeIndexList::get().len() as BalanceOf<T>)
@@ -1435,13 +1441,13 @@ pub mod pallet {
         #[require_transactional]
         fn do_update_exchange_rate() -> DispatchResult {
             let matching_ledger = Self::matching_pool();
-            let total_bonded = Self::get_total_bonded();
+            let total_active_bonded = Self::get_total_active_bonded();
             let issuance = T::Assets::total_issuance(Self::liquid_currency()?);
             if issuance.is_zero() {
                 return Ok(());
             }
             let new_exchange_rate = Rate::checked_from_rational(
-                total_bonded
+                total_active_bonded
                     .checked_add(matching_ledger.total_stake_amount.total)
                     .and_then(|r| r.checked_sub(matching_ledger.total_unstake_amount.total))
                     .ok_or(ArithmeticError::Overflow)?,
