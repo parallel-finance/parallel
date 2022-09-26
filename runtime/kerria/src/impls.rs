65d4e12a5202a9db4de8c4344a04c0e85f70d3b2
MrPai <1164934857@qq.com>
2022-09-26 21:36:55 +0800

    Treausy collect fee

--- runtime/kerria/src/impls.rs
+++ runtime/kerria/src/impls.rs
@@ -18,24 +18,6 @@ pub type NegativeImbalance<T> = <pallet_balances::Pallet<T> as Currency<
     <T as frame_system::Config>::AccountId,
 >>::NegativeImbalance;
 
-/// Logic for the author to get a portion of fees.
-pub struct ToStakingPot<R>(sp_std::marker::PhantomData<R>);
-impl<R> OnUnbalanced<NegativeImbalance<R>> for ToStakingPot<R>
-where
-    R: pallet_balances::Config + pallet_collator_selection::Config,
-    <R as frame_system::Config>::Event: From<pallet_balances::Event<R>>,
-{
-    fn on_nonzero_unbalanced(amount: NegativeImbalance<R>) {
-        let numeric_amount = amount.peek();
-        let staking_pot = <pallet_collator_selection::Pallet<R>>::account_id();
-        <pallet_balances::Pallet<R>>::resolve_creating(&staking_pot, amount);
-        <frame_system::Pallet<R>>::deposit_event(pallet_balances::Event::Deposit {
-            who: staking_pot,
-            amount: numeric_amount,
-        });
-    }
-}
-
 pub struct DealWithFees<R>(sp_std::marker::PhantomData<R>);
 impl<R> OnUnbalanced<NegativeImbalance<R>> for DealWithFees<R>
 where
@@ -47,7 +29,7 @@ where
             if let Some(tips) = fees_then_tips.next() {
                 tips.merge_into(&mut fees);
             }
-            <ToStakingPot<R> as OnUnbalanced<_>>::on_unbalanced(fees);
+            <pallet_treasury::Pallet<R>>::on_unbalanced(fees);
         }
     }
 }
