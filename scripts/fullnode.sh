11cee687c5f2ada3c0a65de5640e6d78b1dbdfea
MrPai <1164934857@qq.com>
2023-04-13 09:26:14 +0800

    fix

--- scripts/fullnode.sh
+++ scripts/fullnode.sh
@@ -18,7 +18,7 @@ PARA_CHAIN="${2:-heiko}"
 RELAY_CHAIN="${3:-kusama}"
 VOLUME="chains"
 NODE_NAME="$1"
-DOCKER_IMAGE="parallelfinance/parallel:v1.9.7"
+DOCKER_IMAGE="parallelfinance/parallel:v1.9.6"
 BASE_PATH="/data"
 
 if [ $# -lt 1 ]; then
