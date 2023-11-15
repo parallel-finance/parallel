72345d632f80a41f9e95cd2c2db0db64deae46c1
MrPai <1164934857@qq.com>
2023-11-15 14:25:39 +0800

    chore: add safety check when update ledger (#1952)
    * chore: add safety check when update ledger
    
    * fix ci

--- pallets/liquid-staking/src/lib.rs
+++ pallets/liquid-staking/src/lib.rs
@@ -936,21 +936,19 @@ pub mod pallet {
 
             Self::do_update_ledger(derivative_index, |ledger| {
                 ensure!(
-                    !Self::is_updated(derivative_index),
+                    !Self::is_updated(derivative_index)
+                        && XcmRequests::<T>::iter().count().is_zero(),
                     Error::<T>::StakingLedgerLocked
                 );
-                let requests = XcmRequests::<T>::iter().count();
                 if staking_ledger.total < ledger.total
                     || staking_ledger.active < ledger.active
                     || staking_ledger.unlocking != ledger.unlocking
-                    || !requests.is_zero()
                 {
                     log::trace!(
                         target: "liquidStaking::set_staking_ledger::invalidStakingLedger",
-                        "index: {:?}, staking_ledger: {:?}, xcm_request: {:?}",
+                        "index: {:?}, staking_ledger: {:?}",
                         &derivative_index,
                         &staking_ledger,
-                        requests,
                     );
                     Self::deposit_event(Event::<T>::NonIdealStakingLedger(derivative_index));
                 }
