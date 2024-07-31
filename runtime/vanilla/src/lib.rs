d4ba9c542f323fff60492bdb9b491f5f6ab70661
0xrjman <rjman.ljm@gmail.com>
2024-07-31 15:01:57 +0800

    Allow non-incremental farming rewards (#1978)
    Co-authored-by: dio-will <ryan@parallel.fi>

--- runtime/vanilla/src/lib.rs
+++ runtime/vanilla/src/lib.rs
@@ -180,7 +180,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("vanilla"),
     impl_name: create_runtime_str!("vanilla"),
     authoring_version: 1,
-    spec_version: 207,
+    spec_version: 208,
     impl_version: 33,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 17,
