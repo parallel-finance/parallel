637d95003fbd9dc62685f1d93fbdc5b38e9d911e
dio-will <ryan@parallel.fi>
2024-08-08 15:38:47 +0800

    Bump to spec 210

--- runtime/kerria/src/lib.rs
+++ runtime/kerria/src/lib.rs
@@ -180,7 +180,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("kerria"),
     impl_name: create_runtime_str!("kerria"),
     authoring_version: 1,
-    spec_version: 209,
+    spec_version: 210,
     impl_version: 33,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 17,
